3b66ef9406fa48dcf9103a354bcc00635c12977c
Wallace Kevellyn <wallace.machadof@gmail.com>
2025-12-03 11:55:10 +0000

    Error: Label is not defined
    src/app/hof/procedures/page.tsx (205:22) @

--- src/app/hof/procedures/page.tsx
+++ src/app/hof/procedures/page.tsx
@@ -29,6 +29,7 @@ import { cn } from "@/lib/utils";
 import { format } from "date-fns";
 import { ptBR } from "date-fns/locale";
 import Image from "next/image";
+import { Label } from "@/components/ui/label";
 
 const productUsedSchema = z.object({
   productName: z.string().min(1, "Selecione um produto."),
